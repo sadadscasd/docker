0c7afd181727c73e5be62b3100c45f42bd16e455
Razyisya <50580765+sadadscasd@users.noreply.github.com>
2023-12-18 17:25:05 +0700

    Update js.js

--- node/js.js
+++ node/js.js
@@ -1,8 +1,1 @@
-const http = require("node:http");
-
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader("Content-Type", "application/json")
-res.end(JSON.stringify({message:"Hello, Docker"}));
-} );
-server.listen(5050,() => console.log("Http server started!"));
+console.log("Hello, world")
